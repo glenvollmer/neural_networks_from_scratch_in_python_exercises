7c76a2d987991bf59b92684fbd6a40779220a628
glenvollmer <glenvollmer@glenvollmers-MacBook-Pro.local>
2023-09-24 16:11:51 -0500

    added layer logic

--- chapter_two/layer_of_neurons.py
+++ chapter_two/layer_of_neurons.py
@@ -1,23 +1,73 @@
 
-def first_neuron():
+def modular_layer():
     '''
-        A single neuron with three inputs, three weights, and a bias
+        Same single layer of neurons as layer one, but modularized logic
     '''
-    inputs = [1, 2, 3]
-    weights = [0.2, 0.8, -0.5]
-    bias = 2
 
-    output = (inputs[0] * weights[0] + 
-              inputs[1] * weights[1] + 
-              inputs[2] * weights[2] + bias)
+    inputs = [1, 2, 3, 2.5]
+    
+    weights = [
+        [0.2, 0.8, -0.5, 1], 
+        [0.5, -0.91, 0.26, -0.5], 
+        [-0.26, -0.27, 0.17, 0.87]
+        ]
+    
+    biases = [2, 3, 0.5]
+
+    layer_outputs = []
+
+    for neuron_weights, neuron_bias in zip(weights, biases):
+        neuron_output = 0
+
+        for n_input, weight in zip(inputs, neuron_weights):
+            neuron_output += n_input * weight
+
+        neuron_output += neuron_bias
+
+        layer_outputs.append(neuron_output)
+
+    return layer_outputs
+
+
+
+def layer_one():
+    '''
+        A single layer of neurons
+    '''
+    inputs = [1, 2, 3, 2.5]
+    weights1 = [0.2, 0.8, -0.5, 1]
+    weights2 = [0.5, -0.91, 0.26, -0.5]
+    weights3 = [-0.26, -0.27, 0.17, 0.87]
+    
+    bias1 = 2
+    bias2 = 3
+    bias3 = 0.5
+
+    output = [
+        inputs[0] * weights1[0] + 
+        inputs[1] * weights1[1] + 
+        inputs[2] * weights1[2] +
+        inputs[3] * weights1[3] + bias1,
+        inputs[0] * weights2[0] + 
+        inputs[1] * weights2[1] + 
+        inputs[2] * weights2[2] +
+        inputs[3] * weights2[3] + bias2,
+        inputs[0] * weights3[0] + 
+        inputs[1] * weights3[1] + 
+        inputs[2] * weights3[2] +
+        inputs[3] * weights3[3] + bias3
+        ]
+    
     return output
 
 
 
 def main():
-    n1 = first_neuron()
+    l1 = layer_one()
+    ml = modular_layer()
     
-    print(f'first neuron: {n1}')
+    print(f'first layer: {l1}')
+    print(f'modular layer: {ml}')
     return
 
 if __name__ == '__main__':
